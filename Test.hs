12e460ba6135960fbb972f0227f5f72785fda61d
damhiya <damhiya@gmail.com>
2022-01-11 00:27:29 +0900

    refactoring Test.hs

--- Test.hs
+++ Test.hs
@@ -4,24 +4,11 @@ import qualified Syntax.Object as O
 import qualified Syntax.Meta as M
 import Typing
 
--- three : [ f : Base -> Base |- Base -> Base ]
--- three = box[f. fn x -> f (f (f x))]
-three :: M.Tm
-three = M.Box [(f, btb)]
-      $ O.Abs x O.Base
-      $ (O.App vf $ O.App vf $ O.App vf vx)
-  where
-    btb = O.Arr O.Base O.Base
-    f = O.Id "f"
-    x = O.Id "x"
-    vf = O.Var f
-    vx = O.Var x
-
 -- close : [ f : Base -> Base |- Base -> Base ] -> [ |- (Base -> Base) -> Base -> Base ]
--- close = fn c -> let box (g.U) = c in box[. fn f -> U with f]
+-- close = fn c -> let box (g.U) = c in box(. fn f -> U with f)
 close :: M.Tm
 close = M.Abs c (M.BoxT [(f, btb)] btb)
-      $ M.LetBox [g] u three
+      $ M.LetBox [g] u vc
       $ M.Box []
       $ O.Abs f btb (O.Meta $ M.Inst u [vf])
   where
@@ -30,13 +17,43 @@ close = M.Abs c (M.BoxT [(f, btb)] btb)
     u = M.GId "U"
     g = O.Id "g"
     f = O.Id "f"
+    vc = M.Var c
     vf = O.Var f
 
--- three' : [ |- (Base -> Base) -> Base -> Base ]
--- three' = close three
-three' :: M.Tm
-three' = M.App close three
+-- three : [ f : Base -> Base, x : Base |- Base ]
+-- three = box(f x. f (f (f x)))
+three :: M.Tm
+three = M.Box [(x, b), (f, btb)]
+      $ (O.App vf $ O.App vf $ O.App vf vx)
+  where
+    btb = O.Arr O.Base O.Base
+    b = O.Base
+    f = O.Id "f"
+    x = O.Id "x"
+    vf = O.Var f
+    vx = O.Var x
 
--- inferType three
--- inferType close
--- inferType three'
+-- three' : [ f : Base -> Base |- Base -> Base ]
+-- three' = let box(f,x.U) = three in box(f. fn x -> U with (f,x))
+three' :: M.Tm
+three' = M.LetBox [x,f] u three
+       $ M.Box [(f, btb)]
+       $ O.Abs x O.Base (O.Meta $ M.Inst u [vx,vf])
+  where
+    btb = O.Arr O.Base O.Base
+    u = M.GId "U"
+    f = O.Id "f"
+    x = O.Id "x"
+    vf = O.Var f
+    vx = O.Var x
+
+-- three'' : [ |- (Base -> Base) -> Base -> Base ]
+-- three'' = close three
+three'' :: M.Tm
+three'' = M.App close three'
+
+-- try these
+_ = inferType close
+_ = inferType three
+_ = inferType three'
+_ = inferType three''
