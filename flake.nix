e48c6c4801fb399e75a2cb83c3d7f46d49cfc5ef
damhiya <damhiya@gmail.com>
2023-05-03 00:12:00 +0900

    update flake input

--- flake.nix
+++ flake.nix
@@ -12,14 +12,14 @@
   outputs = { self, nixpkgs, flake-utils, haskellNix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        index-state = "2022-11-02T00:00:00Z";
+        index-state = "2023-05-01T00:00:00Z";
         overlays = [
           haskellNix.overlay
           (final: prev: {
             MetaLambda = final.haskell-nix.cabalProject' {
               inherit index-state;
               src = ./.;
-              compiler-nix-name = "ghc924";
+              compiler-nix-name = "ghc927";
               shell.tools = {
                 cabal = {
                   inherit index-state;
@@ -27,7 +27,7 @@
                 };
                 haskell-language-server = {
                   inherit index-state;
-                  version = "1.8.0.0";
+                  version = "1.10.0.0";
                 };
                 hpack = {
                   inherit index-state;
